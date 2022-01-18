67fd9483a5e8732ea0c4b9abd98733da904144ca
zxcvhyun <hu3415@naver.com>
2022-01-18 19:24:07 +0900

    add login and logout functionality

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
-  const onLogin = (event) => {
-    authService.login(event.currentTarget.textContent).then(console.log());
+  const navigate = useNavigate();
+
+  const goToMaker = (userId) => {
+    navigate({
+      pathname: '/maker',
+      state: { id: userId },
+    });
   };
+
+  const onLogin = (event) => {
+    authService //
+      .login(event.currentTarget.textContent)
+      .then((data) => goToMaker(data.user.uid));
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.uid);
+    });
+  });
   return (
     <section className={styles.login}>
       <Header />
---
+++ src/components/maker/maker.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import styles from './maker.module.css';
+const Maker = ({ authService }) => {
+  const navigate = useNavigate();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        navigate('/');
+      }
+    });
+  });
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <Footer />
+    </section>
+  );
+};
+
+export default Maker;
